4d1ed95464400250f72f189b82fd2268be572c29
RINITH-AMIN <rinith.amin333@gmail.com>
2024-10-06 15:30:41 +0530

    minor css changes

--- src/app/components/home/bookNow/index.jsx
+++ src/app/components/home/bookNow/index.jsx
@@ -77,9 +77,9 @@ export default function BookNow() {
       title: "Form Submitted!",
       description: "Your booking has been successfully submitted.",
       status: "success",
-      duration: 5000, 
+      duration: 5000,
       isClosable: true,
-      position: "top-right" 
+      position: "top-right",
     });
 
     setDestination("");
@@ -100,100 +100,104 @@ export default function BookNow() {
             objectFit={"cover"}
           />
         </Flex>
-        <form onSubmit={handleSubmit}>
-          <Stack spacing={4}>
-            <Text
-              textTransform={"uppercase"}
-              color={"blue.400"}
-              fontWeight={600}
-              fontSize={"sm"}
-              bg={useColorModeValue("blue.50", "blue.900")}
-              p={2}
-              alignSelf={"flex-start"}
-              rounded={"md"}
-            >
-              Plan Your Journey
-            </Text>
-
-            <FormControl id="whereTo">
-              <FormLabel>Where To</FormLabel>
-              <Select
-                placeholder="Select option"
-                value={destination}
-                onChange={(e) => setDestination(e.target.value)}
-                required={true}
+        <div class="p-7 border-solid border-2 rounded-md">
+          <form onSubmit={handleSubmit}>
+            <Stack spacing={4}>
+              <Text
+                textTransform={"uppercase"}
+                color={"blue.400"}
+                fontWeight={600}
+                fontSize={"sm"}
+                bg={useColorModeValue("blue.50", "blue.900")}
+                p={2}
+                alignSelf={"flex-start"}
+                rounded={"md"}
               >
-                {voyagePackages?.map((pkg) => {
-                  return (
-                    <option key={pkg.place} value={pkg.place}>
-                      {pkg.place}
-                    </option>
-                  );
-                })}
-              </Select>
-            </FormControl>
+                Plan Your Journey
+              </Text>
 
-            <FormControl id="howMany">
-              <FormLabel>How Many People</FormLabel>
-              <Input
-                placeholder="Number of person"
-                value={peopleCount}
-                type="number"
-                onChange={(e) => setPeopleCount(e.target.value)}
-                required={true}
-              />
-            </FormControl>
+              <FormControl id="whereTo">
+                <FormLabel>Where To</FormLabel>
+                <Select
+                  placeholder="Select option"
+                  value={destination}
+                  onChange={(e) => setDestination(e.target.value)}
+                  required={true}
+                >
+                  {voyagePackages?.map((pkg) => {
+                    return (
+                      <option key={pkg.place} value={pkg.place}>
+                        {pkg.place}
+                      </option>
+                    );
+                  })}
+                </Select>
+              </FormControl>
 
-            <FormControl id="startDate">
-              <FormLabel>Start Date</FormLabel>
-              <Input
-                type="date"
-                min={new Date().toISOString().split("T")[0]} // Only future dates
-                value={startDate}
-                onChange={handleStartDateChange}
-                placeholder="Select Start Date"
-                required={true}
-              />
-            </FormControl>
+              <FormControl id="howMany">
+                <FormLabel>How Many People</FormLabel>
+                <Input
+                  placeholder="Number of person"
+                  value={peopleCount}
+                  type="number"
+                  onChange={(e) => setPeopleCount(e.target.value)}
+                  required={true}
+                />
+              </FormControl>
 
-            <FormControl id="endDate">
-              <FormLabel>End Date</FormLabel>
-              <Input
-                min={startDate || new Date().toISOString().split("T")[0]} // Must be after or same as the start date
-                value={endDate}
-                onChange={handleEndDateChange}
-                placeholder="Select End Date"
-                isDisabled={!startDate} // Disable until start date is selected
-                type="date"
-                required={true}
-              />
-            </FormControl>
+              <FormControl id="startDate">
+                <FormLabel>Start Date</FormLabel>
+                <Input
+                  type="date"
+                  min={new Date().toISOString().split("T")[0]} // Only future dates
+                  value={startDate}
+                  onChange={handleStartDateChange}
+                  placeholder="Select Start Date"
+                  required={true}
+                />
+              </FormControl>
 
-            <FormControl id="description">
-              <FormLabel>Description</FormLabel>
-              <Textarea
-                placeholder="Enter at least 50 characters"
-                value={description}
-                minLength={50}
-                maxLength={500}
-                onChange={(e) => setDescription(e.target.value)}
-                required={true}
-              />
-            </FormControl>
+              <FormControl id="endDate">
+                <FormLabel>End Date</FormLabel>
+                <Input
+                  min={startDate || new Date().toISOString().split("T")[0]} // Must be after or same as the start date
+                  value={endDate}
+                  onChange={handleEndDateChange}
+                  placeholder="Select End Date"
+                  isDisabled={!startDate} // Disable until start date is selected
+                  type="date"
+                  required={true}
+                />
+              </FormControl>
 
-            <FormControl id="name" float="right">
-              <Button
-                type="submit"
-                variant="solid"
-                bg="#0D74FF"
-                color="white"
-                _hover={{}}
-              >
-                Book Now
-              </Button>
-            </FormControl>
-          </Stack>
-        </form>
+              <FormControl id="description">
+                <FormLabel>Description</FormLabel>
+                <Textarea
+                  placeholder="Enter at least 50 characters"
+                  value={description}
+                  minLength={50}
+                  maxLength={500}
+                  onChange={(e) => setDescription(e.target.value)}
+                  required={true}
+                />
+              </FormControl>
+
+              <FormControl id="name" float="right">
+                <Button
+                  type="submit"
+                  variant="solid"
+                  bg={"#038aed"}
+                  color="white"
+                  _hover={{
+                    bg: "#004aad",
+                  }}
+                >
+                  Book Now
+                </Button>
+              </FormControl>
+            </Stack>
+          </form>
+        </div>
       </SimpleGrid>
     </Container>
   );
